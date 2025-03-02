b6cd974156470b882686bac245c5589865556ae0
Yuhang <im@jyh.sb>
2025-03-01 18:10:54 -0800

    Update: report script

--- run_report.sh
+++ run_report.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+if [ -n "$IMAGE_VERSION" ]; then
+    echo -e "\033[1;31mWorkspace environment detected. Please conduct the experiment on the pi-cluster.\033[0m"
+    exit 0
+fi
+
 OUTPUT_FILE="result.csv"
 
 if [ -f "$OUTPUT_FILE" ]; then
@@ -10,11 +15,11 @@ if [ -f "$OUTPUT_FILE" ]; then
         ;;
     [Nn]*)
         echo "Aborting."
-        exit 1
+        exit 0
         ;;
     *)
         echo "Invalid input. Aborting."
-        exit 1
+        exit 0
         ;;
     esac
 fi
@@ -30,16 +35,16 @@ fi
 
 echo "N,totalsum,threads,time,speedup" > "$OUTPUT_FILE"
 
-Ns=(10000000 20000000 30000000 40000000 50000000 60000000 70000000 80000000 90000000 100000000)
+Ns=(1024 2048 4096 8192 16384 32768 65536 131072 262144 524288 1048576 2097152 4194304 8388608 16777216 33554432 67108864)
 
-total_runs=30
+total_runs=85
 run_count=1
 
 for N in "${Ns[@]}"; do
 
-    for i in {1..3}; do
+    for i in {1..5}; do
         echo "Experiment ${run_count}/${total_runs}"
-        OUTPUT=$("$tmp_exe" "$N" 10000007 1)
+        OUTPUT=$("$tmp_exe" "$N" 1000007 1)
         echo "$OUTPUT"
 
         while IFS= read -r line; do
